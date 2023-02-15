46e1d9f77995858106d692487d111d3c8a5b1e9f
sloanelauren <sloanelaurenm@gmail.com>
2023-02-14 19:44:58 -0500

    added prices and input for num

--- VendingMachine.java
+++ VendingMachine.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 class VendingMachine{
     public static void main(String[]args)
     {
-        System.out.println("Welcome to the Candy Vending Machine! Please enter the number of the snack you want!");
-        System.out.println("|   1. CandyCorn    |");
-        System.out.println("|   2. KitKats      |");
-        System.out.println("|   3. Reeses       |");
-        System.out.println("|   4. Skittles     |");
-        System.out.println("|   5. Starburts    |");
-        System.out.println("|   6. Twix         |");
-        System.out.println("|   7. Swedish Fish |");
-        System.out.println("|   8. Snickers     |");
-        System.out.println("|   9. M&M's        |");
-        System.out.println("|   10. 3 Musketeers|");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Candy Vending Machine! Please enter the number (1-10) of the snack you want!");
+        System.out.println("|   1. CandyCorn  - $1.00    |");
+        System.out.println("|   2. KitKats    - $1.00    |");
+        System.out.println("|   3. Reeses     - $1.00    |");
+        System.out.println("|   4. Skittles   - $1.00    |");
+        System.out.println("|   5. Starburts  - $1.00    |");
+        System.out.println("|   6. Twix       - $1.00    |");
+        System.out.println("|   7. Swedish Fish - $1.00  |");
+        System.out.println("|   8. Snickers   - $1.00    |");
+        System.out.println("|   9. M&M's      - $1.00    |");
+        System.out.println("|   10. 3 Musketeers - $5.00 |");
+
+        int num = sc.nextInt();
     }
 }
