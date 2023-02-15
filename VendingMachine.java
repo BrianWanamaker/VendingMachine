5d3b042110dd8cabdc2e9f5f05acada734640bc1
sloanelauren <sloanelaurenm@gmail.com>
2023-02-14 19:29:18 -0500

    Added welcome message and beginning instructions

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,6 +1,6 @@
 class VendingMachine{
     public static void main(String[]args)
     {
-        
+        System.out.println("Welcome to the Candy Vending Machine! Please enter the number of the snack you want!");
     }
 }
