06b7641142dc5eb269601ce0d80d48d6a4857cb1
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2023-02-14 19:37:36 -0500

    Prints out all snacks

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 class VendingMachine{
     public static void main(String[]args)
     {
         System.out.println("Welcome to the Candy Vending Machine! Please enter the number of the snack you want!");
+        System.out.println("|   1. CandyCorn    |");
+        System.out.println("|   2. KitKats      |");
+        System.out.println("|   3. Reeses       |");
+        System.out.println("|   4. Skittles     |");
+        System.out.println("|   5. Starburts    |");
+        System.out.println("|   6. Twix         |");
+        System.out.println("|   7. Swedish Fish |");
+        System.out.println("|   8. Snickers     |");
+        System.out.println("|   9. M&M's        |");
+        System.out.println("|   10. 3 Musketeers|");
     }
 }
