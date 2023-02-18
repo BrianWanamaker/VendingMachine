fec2bc885ad53950f1f692aee0303dba1512ef46
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2023-02-18 17:14:25 -0500

    Confirmation message before purcahse

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -25,35 +26,87 @@ class VendingMachine{
         System.out.println("|____________________________|");
 
         int num = sc.nextInt();
-        Map<Integer, String> vendingItems  = new HashMap<Integer, String>();
-        vendingItems.put(1, "Candycorn");
-        vendingItems.put(2, "KitKats");
-        vendingItems.put(3, "Reeses");
-        vendingItems.put(4, "Skittles");
-        vendingItems.put(5, "Starburst");
-        vendingItems.put(6, "Twix");
-        vendingItems.put(7, "Swedish Fish");
-        vendingItems.put(8, "Snickers");
-        vendingItems.put(9, "M&M's");
-        vendingItems.put(10, "3 Musketeers");
-        System.out.println("You choose: " + vendingItems.get(num));
+        num-=1;
+        ArrayList<candy> candy = new ArrayList<candy>();
+        candy.add(new candy(1,"Candycorn", 2));
+        candy.add(new candy(2,"KitKats", 2));
+        candy.add(new candy(3,"Reeses", 1));
+        candy.add(new candy(4,"Skittles", 3));
+        candy.add(new candy(5,"Starburst", 2));
+        candy.add(new candy(6,"Twix", 1));
+        candy.add(new candy(7,"Swedish Fish", 1));
+        candy.add(new candy(8,"Snickers", 1));
+        candy.add(new candy(9,"M&M's", 4));
+        candy.add(new candy(10,"3 Musketeers", 5));
 
+        System.out.println("You chose " + candy.get(num).getItem() + " for $"  + candy.get(num).getPrice());
        
         System.out.println("Insert payment. $1 bills only.");
          while (money){
               currentPay = sc.nextInt();
             if (currentPay != 1) {
-                System.out.println("Please enter $1");
-            } else if (balance <= 5) {
+                System.out.println("Please enter $1");      
+            } 
+            else if(balance >= candy.get(num).getPrice()-1)
+            {
+                balance+=1;
+                System.out.println("Thank you for your payment.");
+                System.out.println("You've inserted $" + balance);
+                System.out.println("Would you like to enter more money?(Y OR N)");
+                String input = sc.next();
+                if(input.toLowerCase().equals("n"))
+                {
+                    money = false;
+                }
+                else
+                {
+                    System.out.println("Insert payment. $1 bills only.");
+                }           
+            }
+            else 
+            {
                 System.out.println("Please enter more money.");
                 balance += 1;
-                System.out.println("You've inserted " + balance);
-            } 
-            else if(balance == num){
-                System.out.println("Thank you for your payment.");
-                money = false;
-            }
+                System.out.println("You've inserted $" + balance);
+                System.out.println("You owe $" + (candy.get(num).getPrice() - balance) + " more");
+            }       
          }
-
+         System.out.println();
+         System.out.println("You selected " + candy.get(num).getItem() + " for $"  + candy.get(num).getPrice());
+         System.out.println("You have inserted $" + balance);
+         System.out.println("Would you like to finish the purchase?(Y or N)");
+         String input = sc.next();
+         if(input.toLowerCase().equals("n"))
+         {
+            System.out.println("You have been refunded $" + balance);
+         }
+         else
+         {
+            // give exact change 
+         }
+    }
+}
+class candy
+{
+    String item;
+    int numberOfItem;
+    int price;
+    candy( int numberOfItem,String item,int price)
+    {
+        this.item=item;
+        this.numberOfItem=numberOfItem;
+        this.price= price;
+    }
+    public String getItem()
+    {
+        return item;
+    }
+    public int getNumberOfItem()
+    {
+        return numberOfItem;
+    }
+    public int getPrice()
+    {
+        return price;
     }
 }
